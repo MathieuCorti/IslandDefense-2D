f97a0367dbec93b67898e68925f7aee02daba65c
Wimps <mathieucorti@gmail.com>
2018-03-12 11:55:56 +1100

    Minors changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+//
+//  main.cpp
+//  IslandDefense
+//
+//  Created by Mathieu Corti on 3/12/18.
+//
+
 #include <stdlib.h>
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/OpenGL.h>
 #include <GLUT/glut.h>
+#include <iostream>
 
 
 void display() {
@@ -13,8 +21,33 @@ void display() {
     glutSwapBuffers();
 }
 
-/* You can ignore this for now, it just lets you exit when you press 'q' or ESC */
+/**
+ *
+ * @param key
+ * @param x
+ * @param y
+ *
+ * F1: Toggle wireframe mode on/off
+ * n: Toggle normal visualisation on/off
+ * q: Left cannon rotate up
+ * Q: Left cannon rotate down
+ * a: Left boat move left
+ * d: Left boat move right
+ * o: Right cannon rotate up
+ * O: Right cannon rotate down
+ * l: Right boat move leftj: Right boat move right
+ * e: Left boat fire
+ * i: Right boat fire
+ * r: Left boat defence
+ * y: Right boat defence
+ * g: Toggle wave animation on/off
+ * +: Double wave tesselation
+ * -: Halve wave tesselation
+ * q/Esc: Quit the program
+ *
+ */
 void keyboard(unsigned char key, int x, int y) {
+    std::cout << "Key : " << std::endl;
     switch (key) {
         case 27:
         case 'q':
@@ -28,7 +61,7 @@ void keyboard(unsigned char key, int x, int y) {
 int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("Tutorial 1");
+    glutCreateWindow("Island Defense 2D");
 
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
